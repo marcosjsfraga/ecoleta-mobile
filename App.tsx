3e80c82a25f17dcdc7bb9129c6c2f3d5847fce62
Marcos Fraga <marcosjsfraga@gmail.com>
2020-06-04 22:44:26 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo'
+import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_500Medium, useFonts } from '@expo-google-fonts/roboto'
+
+import Home from './src/pages/Home'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const fontsLoaded = useFonts({
+        Roboto_400Regular,
+        Roboto_500Medium
+    })
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
+
+    return (
+        <>
+            <StatusBar barStyle="dark-content" backgroundColor="transparent" trans/>
+            <Home />
+        </>
+    );
+}
